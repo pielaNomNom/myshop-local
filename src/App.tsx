f35bd134cb7a44a08db1e24a82154eeb5920b3a8
Hydro <pielawojciech06@gmail.com>
2025-04-02 21:45:38 +0200

    chore: add App, index files and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-function App() {
+import { BrowserRouter as Router } from "react-router-dom";
+
+import styles from "./App.scss"
+
+export function App() {
     return (
-        <>
-        </>
+        <Router>
+            <div>Header</div>
+            <main className={styles.container}></main>
+        </Router>
     );
 }
